80b45bf8839be2f5c6f168e6fc0ca30041fd7a93
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-04-05 03:08:09 +0200

    conditional statements

---
+++ conditionalstatements.py
@@ -0,0 +1,17 @@
+greetings = 'Hello World!'
+bye = 'Bye World'
+check = False
+checkToo = True
+
+if check:
+  print(greetings)
+else: 
+  print(bye)
+
+if check:
+  print('Hello World')
+elif checkToo:
+  print('This is my world')
+else:
+  print('Bye World')
+  
