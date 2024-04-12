e5725fa1b988e09d1734e6a76c61710878d5af13
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-04-12 06:36:21 +0200

    nested for loops

---
+++ learning-with-josh/nestestes_for_loops.py
@@ -0,0 +1,12 @@
+
+for x in range(4):
+  for y in range(4):
+    print('(%i, %i)' %(x, y))
+    
+
+#challenge
+numbers = [ 5, 2, 5, 2, 2 ]
+
+for x in numbers:
+  for y in range(1):
+    print('x' * x)
