471a29a3562742d2e7def785453b6e5692d91218
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-04-18 04:11:08 +0200

    classes

---
+++ learning-with-josh/classes.py
@@ -0,0 +1,12 @@
+
+class Point:
+  def multiply(self, num, num2):
+    print(num * num2)
+  
+  def add(self, num, num2):
+    print(num + num2)
+    
+point1 = Point()
+
+point1.add(2, 5)
+point1.multiply(2, 5)
