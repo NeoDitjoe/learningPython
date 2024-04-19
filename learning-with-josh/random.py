d9c3d9de32815370a8d82fb153dcb89c9c526ceb
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-04-19 06:18:59 +0200

    random

---
+++ learning-with-josh/random.py
@@ -0,0 +1,29 @@
+# #random
+import random
+
+keeper = 0
+keeper2 = 0
+choice = ['John', 'Mary', 'Bob', "Cozy"]
+
+for i in range(4):
+  keeper+= random.random()
+  keeper2+= random.randint(100, 1000) 
+  
+  
+print(keeper)
+print(keeper2)
+print(random.choice(choice))
+
+# challenge
+
+class Dice:
+  def roll(self):
+    random_Int = random.randint(1, 6)
+    random_IntB = random.randint(1, 6)
+    
+    return (random_Int, random_IntB)
+
+
+dice = Dice()
+
+print(dice.roll())
